6c4c92d0ca6824428ffa6dce35497a8ddca43aed
liusudo123 <2534160816@qq.com>
2019-09-13 11:42:48 +0800

    9.配置本地域名    1.vi/etc/hosts    2. 127.0.0.1 www.meiduo.site    3. dev.py---->ALLOWED_HOSTS = ['www.meiduo.site']

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'trtu0x2@flpyctr_eqlgwpidoq&)*=&7ac#=kf#-6xsjnx$_lt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.meiduo.site']
 
 
 # Application definition
