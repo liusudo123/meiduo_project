3fb0386b35e3ac0fc598e302030065eecceccbf8
liusudo123 <2534160816@qq.com>
2019-09-14 17:16:42 +0800

    11.创建子应用 apps----users    1. dev----'apps.users',    2. 总---子----类是图-- render

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('apps.users.urls')),
+    url(r'register/$', views.Register.as_view())
+
 ]
