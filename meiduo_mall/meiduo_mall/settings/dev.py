fc965ddd2d2c0f2c860d3b250d5a32156080285d
liusudo123 <2534160816@qq.com>
2019-09-15 19:34:40 +0800

    5.captcha 文件夹---libs---测试  配置redis 的存储图片验证码 数据库2号

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -144,6 +144,16 @@ CACHES = {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     },
+
+    # 2数据库
+    "verify_image_code": {  # session
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+
 }
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "session"
