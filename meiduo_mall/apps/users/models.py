342def2cab9935b6c536bc3b44e7bc877dde201f
liusudo123 <2534160816@qq.com>
2019-09-18 19:48:49 +0800

    ---------第四天-------- 1.新增了 users---models.py--->email_active

--- meiduo_mall/apps/users/models.py
+++ meiduo_mall/apps/users/models.py
@@ -10,6 +10,10 @@ class User(AbstractUser):
     """自定义用户模型类"""
     mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
 
+    # 新增字段
+    email_active = models.BooleanField(default=False, verbose_name='激活邮箱')
+
+
     class Meta:
         db_table = 'tb_users'
         verbose_name = '用户'
