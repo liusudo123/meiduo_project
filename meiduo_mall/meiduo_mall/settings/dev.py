dffd040722e4bb7f32c81837be9dc21810f54101
liusudo123 <2534160816@qq.com>
2019-09-18 16:34:45 +0800

    10.qq登录--获取请求登录网址   1./qq/login/ --- from QQloginTool.QQTool import QAuthQQ   2.OAuthQQ()  from django.conf import settings   3.login_url = oatuth.get_qq_loginur()

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -220,3 +220,8 @@ AUTHENTICATION_BACKENDS = ['apps.users.utils.UsernameMobileAuthBackend']
 
 
 LOGIN_URL = '/login/'
+
+# qq登录
+QQ_CLIENT_ID = '101518219'
+QQ_CLIENT_SECRET = '418d84ebdc7241efb79536886ae95224'
+QQ_REDIRECT_URI = 'http://www.meiduo.site:8000/oauth_callback'
