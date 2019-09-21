ce787f2e78366344a2758b855e3286406a0918a6
liusudo123 <2534160816@qq.com>
2019-09-21 18:13:01 +0800

    5.获取广告数据

--- meiduo_mall/apps/contents/views.py
+++ meiduo_mall/apps/contents/views.py
@@ -5,18 +5,28 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
+from apps.contents.models import ContentCategory
 from apps.contents.utils import get_categories
 from apps.goods.models import GoodsChannel
 
 
 class IndexView(View):
     def get(self, request):
-       # 1.商品分类, 数据显示
+        # 1.商品分类, 数据显示
         categories = get_categories()
-       # 广告数据显示
+        #2 广告数据显示
+        contents = {}
+
+        # 2.1 获取所有的广告分类
+        ad_categories = ContentCategory.objects.all()
+
+        # 2.2 遍历所有的广告分类--->对应的广告内容
+        for ad_cat in ad_categories:
+            contents[ad_cat.key] = ad_cat.content_set.filter(status=True).order_by('sequence')
 
         context = {
-            'categories': categories
+            'categories': {},
+            'contents': contents
         }
-        # print(categories)
+        print(context)
         return render(request, 'index.html', context)
