e575d20c9b89ad3c50f9d608392dcb71df2b05a7
liusudo123 <2534160816@qq.com>
2019-09-22 16:53:47 +0800

    --------------第7天------------- 详情页替换
    Signed-off-by: liusudo123 <2534160816@qq.com>

--- meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/apps/goods/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view(), name='list'),
 
     # 热销排行# /hot/(?P<category_id>\d+)/
-    url(r'^hot/(?P<category_id>\d+)/$', views.HotView.as_view())
+    url(r'^hot/(?P<category_id>\d+)/$', views.HotView.as_view()),
+    #  详情页/detail/(?P<sku_id>\d+)/
+    url(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view(), name='detail'),
 
 ]
